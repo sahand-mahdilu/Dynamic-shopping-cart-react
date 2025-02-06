92dff28d78c2762f9ea98793c0c10c67934e60b2
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-06 13:20:49 +0330

    feat : props added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <div className='flex justify-evenly items-center pt-5 '>
-     <Cart></Cart>
+   <Cart image="./public/img/phone-2.jpg" name="Xiaomi" description="ALI ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit"/>
       
 
     </div>

5e2a29bb7153f188f9e15e6184c67654dbd9819a
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-06 13:25:12 +0330

    feat : destructureing added

--- src/Cart-component/Cart.jsx
+++ src/Cart-component/Cart.jsx
@@ -1,5 +1,9 @@
 
-export default function Cart() {
+export default function Cart(props) {
+
+   
+    let {image, name, description}=props
+
   return (
     <div className="Cart_container max-w-56 bg-gray-200 rounded-xl ">
 
@@ -8,11 +12,11 @@ export default function Cart() {
         <div className="cart flex flex-col items-center justify-center">
 
                 <div className="imgContainer w-48 h-48">
-                    <img className="w-full h-full" src="./public/img/phone-1.jpg" alt="" />
+                    <img className="w-full h-full" src={image} alt="img" />
                 </div>
                <div className="px-2">
-               <h2 className="text-xl font-bold text-center pt-3">SAMSUNG</h2>
-               <p className="pt-5 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit</p>
+               <h2 className="text-xl font-bold text-center pt-3">{name}</h2>
+               <p className="pt-5 text-center">{description}</p>
                </div>
 
                 <button className="bg-blue-600 text-white p-2 w-full mt-8 hover:bg-blue-400" >Add to Cart</button>

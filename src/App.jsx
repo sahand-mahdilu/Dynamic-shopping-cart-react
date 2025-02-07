ac99121e2994bbac6e3f44fc53daad0cca2faeae
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-07 21:03:57 +0330

    feat : image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-
-import './App.css'
-import Cart from './Cart-component/Cart'
+import "./App.css";
+import Cart from "./Cart-component/Cart";
 
 function App() {
+  const products = [
+    {
+      name: "Xiaomi",
+      description:
+        "ALI ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit",
+      image: "./public/img/phone-1.jpg",
+    },
 
-  const products=[
-    {name:"Xiaomi", description:"ALI ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit", image:"./public/img/phone-1.jpg"},
+    {
+      name: "Samsaung",
+      description:
+        "REZA ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit",
+        image: "./public/img/phone-2.jpg",
+    },
 
-    {name:"Samsaung", description:"REZA ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit", },
-
-    {name:"Nokia", description:"MAHDI ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit", image:"./public/img/phone-3.jpg"},
-  ]
+    {
+      name: "Nokia",
+      description:
+        "MAHDI ipsum dolor sit amet consectetur adipisicing elit. Distinctio exercit",
+      image: "./public/img/phone-3.jpg",
+    },
+  ];
 
   return (
     <>
-    <div className='flex justify-evenly items-center pt-5 '>
-   <Cart {...products[0]} >
-
-    <h2 className='text-xl font-bold text-red-500'>30% Off</h2>
-
-   </Cart>
-   <Cart {...products[1]} ></Cart>
-   <Cart {...products[2]} ></Cart>
-      
-
-    </div>
+      <div className="flex justify-evenly items-center pt-5 ">
+        <Cart {...products[0]}>
+          <h2 className="text-xl font-bold text-red-500">30% Off</h2>
+        </Cart>
+        <Cart {...products[1]}></Cart>
+        <Cart {...products[2]}></Cart>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Cart-component/Cart.jsx
+++ src/Cart-component/Cart.jsx
@@ -1,33 +1,30 @@
-
-export default function Cart({image="./public/img/no-img.png", name, description,children}) {
-
-
-   
-
+export default function Cart({
+  image = "./public/img/no-img.png",
+  name,
+  description,
+  children,
+}) {
   return (
     <div className="Cart_container max-w-56 bg-gray-200 rounded-xl ">
+      <h1 className="title text-center pb-1 text-gray-800 text-lg font-semibold">
+        Product Cart
+      </h1>
 
-        <h1 className="title text-center pb-1 text-gray-800 text-lg font-semibold">Product Cart</h1>
-
-        <div className="cart flex flex-col items-center justify-center">
-
-                <div className="imgContainer w-48 h-48">
-                    <img className="w-full h-full" src={image} alt="img" />
-                </div>
-               <div className="px-2">
-               <h2 className="text-xl font-bold text-center pt-3">{name}</h2>
-               <p className="pt-5 text-center">{description}</p>
-               </div>
-
-               {children}
-
-                <button className="bg-blue-600 text-white p-2 w-full mt-8 hover:bg-blue-400" >Add to Cart</button>
-               
-
-
-
+      <div className="cart flex flex-col items-center justify-center">
+        <div className="imgContainer w-48 h-48">
+          <img className="w-full h-full" src={image} alt="img" />
+        </div>
+        <div className="px-2">
+          <h2 className="text-xl font-bold text-center pt-3">{name}</h2>
+          <p className="pt-5 text-center">{description}</p>
         </div>
 
+        {children}
+
+        <button className="bg-blue-600 text-white p-2 w-full mt-8 hover:bg-blue-400">
+          Add to Cart
+        </button>
+      </div>
     </div>
-  )
+  );
 }
